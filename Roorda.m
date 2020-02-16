6537b8c432a854cfd394fe30ae163a28f0b977b8
Kaccie Y Li <kaccie@berkeley.edu>
2020-02-15 21:23:54 -0800

    add convex-hull based method to isolate spot field from background in "process" method

--- Roorda.m
+++ Roorda.m
@@ -3,12 +3,21 @@ classdef Roorda < handle
     %   MKS units assumed
     
     properties (Constant, Access = private)
-        lensletFocalLength = 7.6e-3
+        lensletFocalLength = 7.6e-3 % meters
+        lensletPitch = 0.3e-3 % meters
+        pixelPitch = 7.5e-6 % meters
+        magnification = 0.8
+        
+        scaleForProcessing = 0.25
+        thresholdSensitivity = 0.3
+        theta = linspace(0, 2*pi, 360)
     end
     
-    properties (Access = private)
+    properties %(Access = private)
         bkgd % background image
         meas % measurment image w/ background subtracted
+        
+        pupil = struct('x', 0, 'y', 0, 'r', 0) % pixels
     end
     
     methods (Access = public)
@@ -26,12 +35,28 @@ classdef Roorda < handle
             outputArg = 5;
             
             obj.meas = imsubtract(rawMeasurement, obj.bkgd);
+            
+            meas_scaled = imresize(obj.meas, obj.scaleForProcessing,...
+                "AntiAliasing", true);
+            meas_bin = imbinarize(meas_scaled, "adaptive",...
+                "Sensitivity", obj.thresholdSensitivity);
+            [y, x] = find(meas_bin);
+            [c, r] = minboundcircle(x, y);
+            obj.pupil.x = c(1) / obj.scaleForProcessing;
+            obj.pupil.y = c(2) / obj.scaleForProcessing;
+            obj.pupil.r = r / obj.scaleForProcessing;
         end
         
         function show(obj)
+            x = obj.pupil.x + obj.pupil.r * cos(obj.theta);
+            y = obj.pupil.y + obj.pupil.r * sin(obj.theta);
+            
             imagesc(obj.meas)
+            hold on
+            plot(x, y, 'g')
             axis off
             colormap gray
+            hold off
         end
         
     end
